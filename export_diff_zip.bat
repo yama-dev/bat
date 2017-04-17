f82c45c16a47e558645aee9aae0462c412297ab8
yama-dev <tatsuya.yamamoto69@gmail.com>
2017-04-17 13:32:49 +0900

    add export_diff_zip.bat

---
+++ export_diff_zip.bat
@@ -0,0 +1,17 @@
+rem Gitで差分ファイルをZip圧縮するバッチファイル
+
+if "%2" EQU "" (
+  set PARAM1=HEAD
+  set PARAM2=%1
+) else (
+  set PARAM1=%1
+  set PARAM2=%2
+)
+ 
+setlocal enabledelayedexpansion
+set RET_DIR=
+for /F "usebackq" %%i in (`git diff --name-only %PARAM1% %PARAM2%`) do (
+  set RET_DIR=!RET_DIR! "%%i"
+)
+ 
+git archive --format=zip --prefix=archive/ %PARAM1% %RET_DIR% -o ./diff.zip
