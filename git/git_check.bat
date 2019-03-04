ebe92fd59fcc1da468e9bf4e7761be92ced74917
yama-dev <tatsuya.yamamoto69@gmail.com>
2019-03-04 19:45:34 +0900

    gitの設定を確認するバッチを追加

---
+++ git/git_check.bat
@@ -0,0 +1,8 @@
+@echo off
+git config --global --list
+rem
+rem git config --global http.proxy http://proxy.co.jp:8080
+rem git config --global https.proxy http://proxy.co.jp:8080
+rem git config --global url."https://".insteadOf git://
+rem
+cmd /k
