1f208124d80ec1a5d212c5f049efcabfba20f0a8
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-01-23 21:52:54 +0900

    adjust profile file.

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,9 +4,31 @@
 $a = (Get-Host).UI.RawUI 
 $a.WindowTitle = "Powershell"
 
+# set directory
+$desktop = "C:\Users\kirksville\Desktop"
+$work = "C:\Users\kirksville\Desktop\_work"
+$storage = "C:\Users\kirksville\Desktop\_storage"
+
 Set-Location "D:\works"
 
 Write-Host "policy -> " (Get-ExecutionPolicy) -ForegroundColor "Gray"
+
+# goto desktop
+function desktop(){
+  cd $desktop
+}
+
+# goto working-directory
+function work(){
+  cd $work
+}
+
+# make tree-file
+function maketree(){
+  $_path = (Convert-Path .)
+  $_date = Get-Date -Format "yyyy-MMdd-HHmmss"
+  tree $_path /F > ../tree_$_date.md
+}
 
 function rmrf {
   # This is an equivalent command of "rm -rf" in Unix-like systems.
